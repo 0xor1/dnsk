77559731939ad648fa69547d951bc14ff48261f1
dan7c2 <dan7c2@gmail.com>
2025-07-24 16:09:29 +0100

    update to latest common

--- Dnsk.I18n/SZLibrary.g.cs
+++ Dnsk.I18n/SZLibrary.g.cs
@@ -1,7 +1,7 @@
 // Generated Code File, Do Not Edit.
 // This file is generated with Common.Cli.
 // see https://github.com/0xor1/common/blob/main/Common.Cli/I18n.cs
-// executed with arguments: i18n <abs_file_path_to>/Dnsk.I18n Dnsk.I18n true 
+// executed with arguments: i18n <abs_file_path_to>/Dnsk.I18n Dnsk.I18n false 
 
 using Common.Shared;
 
@@ -9,8 +9,8 @@ namespace Dnsk.I18n;
 
 public static partial class S
 {
-    private static readonly IReadOnlyDictionary<string, IReadOnlyDictionary<string, TemplatableString>> Library =
-        new Dictionary<string, IReadOnlyDictionary<string, TemplatableString>>()
+    private static readonly Dictionary<string, Dictionary<string, TemplatableString>> Library =
+        new ()
         {
             
             { Common.Shared.I18n.S.EN, EN_Strings },
