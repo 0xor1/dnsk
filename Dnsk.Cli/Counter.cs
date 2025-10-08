6ec95e4c2f18c450c565b4608661661015885a7f
dan7c2 <dan7c2@gmail.com>
2025-10-08 17:27:37 +0100

    pass ctkn

--- Dnsk.Cli/Counter.cs
+++ Dnsk.Cli/Counter.cs
@@ -14,17 +14,17 @@ public class Counter
     /// <summary>
     /// Increment your counter
     /// </summary>
-    public async Task Increment() => Io.WriteYml(await _api.Counter.Increment());
+    public async Task Increment(CancellationToken ctkn = default) => Io.WriteYml(await _api.Counter.Increment(ctkn));
 
     /// <summary>
     /// Decrement your counter
     /// </summary>
-    public async Task Decrement() => Io.WriteYml(await _api.Counter.Decrement());
+    public async Task Decrement(CancellationToken ctkn = default) => Io.WriteYml(await _api.Counter.Decrement(ctkn));
 
     /// <summary>
     /// Get a users counter, defaults to yours
     /// </summary>
     /// <param name="user">-u, The user id to get the counter for</param>
-    public async Task Get(string? user = null) =>
-        Io.WriteYml(await _api.Counter.Get(new() { User = user }));
+    public async Task Get(string? user = null, CancellationToken ctkn = default) =>
+        Io.WriteYml(await _api.Counter.Get(new() { User = user }, ctkn));
 }
